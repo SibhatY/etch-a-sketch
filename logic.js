706307f59e302c68cf977d95e2b1a78bc2fca8ef
SibhatY <57141058+SibhatY@users.noreply.github.com>
2024-06-20 13:02:17 -0500

    Addition of resizing logic with resizing button

--- logic.js
+++ logic.js
@@ -3,19 +3,48 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const gridContainer = document.querySelector('#grid-container');
 
-    const totalSquares = 16 * 16;
+    generateGrid(16);
 
-    for (let i = 0; i < totalSquares; i++) {
 
-        const square = document.createElement('div');
+    document.querySelector(`#resize-button`).addEventListener(`click`, function() {
 
-        square.classList.add('square');
+        let squaresSide = prompt("Enter number of squares-per-side for a new grid. No more than 100!", 0);
+        squaresSide = parseInt(squaresSide);
 
-        square.addEventListener(`mouseover`, function() {
-            square.classList.add(`square-hover`);
-        });
+        if (squaresSide > 0 && squaresSide <= 100) {
+
+            generateGrid(squaresSide);
+        }
+        else {
+
+            alert("Number should be between 1 and 100!");
+        }
+    });
+
+    function generateGrid(squaresSide) {
+
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
+
+        const squareSize = 100 / squaresSide;
         
-        gridContainer.appendChild(square);
-        
+
+
+        for (let i = 0; i < squaresSide*squaresSide; i++) {
+
+            const square = document.createElement(`div`);
+            square.classList.add(`square`);
+            square.style.width = `${squareSize}%`;
+            square.style.height = `${squareSize}%`;
+
+            square.addEventListener(`mouseover`, function() {
+
+                square.classList.add(`square-hover`);
+            });
+            gridContainer.appendChild(square);
+        }
     }
+
+
 });
